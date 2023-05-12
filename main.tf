6799cc60589b08267bf4473e454364359baf6a22
venkat <venkat.mobiqa@gmail.com>
2023-05-12 09:25:55 +0530

    updating ./tf-module-subnets2

--- main.tf
+++ main.tf
@@ -9,3 +9,23 @@ resource "aws_subnet" "main" {
     { Name = "${var.env}-${var.name}-subnet-${count.index + 1}" }
   )
 }
+
+
+resource "aws_route_table" "example" {
+  vpc_id = var.vpc_id
+
+#  route {
+#    cidr_block = "10.0.1.0/24"
+#    gateway_id = aws_internet_gateway.example.id
+#  }
+#
+#  route {
+#    ipv6_cidr_block        = "::/0"
+#    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+#  }
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-${var.name}-route_table" }
+  )
+}
