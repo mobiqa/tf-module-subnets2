1259d96536f19b9eab65dc3b98ec3d6c25eb9de1
venkat <venkat.mobiqa@gmail.com>
2023-05-12 09:39:51 +0530

    updating ./tf-module-subnets2

--- main.tf
+++ main.tf
@@ -14,15 +14,11 @@ resource "aws_subnet" "main" {
 resource "aws_route_table" "example" {
   vpc_id = var.vpc_id
 
-#  route {
-#    cidr_block = "10.0.1.0/24"
-#    gateway_id = aws_internet_gateway.example.id
-#  }
-#
-#  route {
-#    ipv6_cidr_block        = "::/0"
-#    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
-#  }
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = var.vpc_peering_connection_id
+  }
+
 
   tags = merge(
     local.common_tags,
